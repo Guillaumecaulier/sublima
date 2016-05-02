aecdd5f8a42a7636ecd0130c5d13e18391dc821b
Guillaume Caulier <cauliergui@gmail.com>
2016-05-02 22:58:50 +0200

    templ

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,25 @@
-//= require_tree .
+//= require jquery-1.11.2.min
+
+//= require plugin/jquery.easing
+//= require jquery-ui.min
+//= require bootstrap.min
+
+//= require plugin/jquery.flexslider
+//= require plugin/background-check.min
+//= require plugin/jquery.fitvids
+//= require plugin/jquery.viewportchecker
+//= require plugin/jquery.stellar.min
+//= require plugin/wow.min
+//= require plugin/jquery.colorbox-min
+//= require plugin/owl.carousel.min
+//= require plugin/isotope.pkgd.min
+//= require plugin/masonry.pkgd.min
+//= require plugin/imagesloaded.pkgd.min
+//= require plugin/jPushMenu
+//= require plugin/jquery.fs.tipper.min
+//= require plugin/mediaelement-and-player.min
+//= require plugin/jquery.validate.min
+//= require theme
+//= require navigation
+//= require plugin/jquery.singlePageNav
+//= require contact-form
